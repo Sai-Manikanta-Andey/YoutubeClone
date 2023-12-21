dcb1461f4ab0a58a977132c45ae1588c39d8de74
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2023-12-21 13:23:51 +0530

    sidebar complete

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,16 +2,32 @@ import React from "react";
 import { LuMenu } from "react-icons/lu";
 import { CgProfile } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
     <div className="flex justify-between items-center p-4 shadow-md">
       <div className="flex gap-4 items-center">
-        <LuMenu fontSize={25} />
+        <LuMenu
+          fontSize={25}
+          className="cursor-pointer "
+          onClick={toggleMenuHandler}
+        />
         <img src="logo.png" alt="logo" width={120} />
       </div>
       <div className="flex justify-between items-center gap-4 border-2 border-gray-400 rounded-3xl p-2 lg:w-4/12">
-        <input type="text" placeholder="Search" className="px-2 outline-none lg:w-[90%]" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="px-2 outline-none lg:w-[90%]"
+        />
         <button>
           <CiSearch fontSize={20} />
         </button>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,35 +1,92 @@
 import React from "react";
+import { CgProfile } from "react-icons/cg";
+import { useSelector } from "react-redux";
 
 const SideBar = () => {
-  return (
-    <div className="w-36 ">
-      <div>
-        <ul>
-          <li>Music</li>
-          <li>Sports</li>
-          <li>Gaming</li>
-          <li>Movies</li>
+  const isMenuOpen = useSelector(state => state.app.isMenuOpen)
+  return ( isMenuOpen?
+    (<div className="w-36  ">
+      <div className="py-4">
+        <ul className="flex flex-col gap-4">
+          <li className="flex gap-4 items-center">
+            <span>
+              <img src="./home.svg" alt="home" />
+            </span>
+            Home
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./shorts.svg" alt="shorts" />
+            </span>
+            Shorts
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./subscriptions.svg" alt="subscription" />
+            </span>
+            Subscriptions
+          </li>
         </ul>
       </div>
       <hr />
-      {/* <div>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+
+      {/* YOU */}
+
+      <div className=" py-4">
+        <h3 className="font-semibold text-xl">You</h3>
+        <ul className="flex flex-col gap-4 mt-2">
+          <li className="flex gap-4 items-center">
+            <span>
+              {" "}
+              <CgProfile fontSize={23} opacity={0.8} />{" "}
+            </span>
+            Your channel
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./history.svg" alt="history" />
+            </span>
+            History
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./you.svg" alt="you" />
+            </span>
+            Your videos
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./watchlater.svg" alt="watchlater" />
+            </span>
+            Watch later
+          </li>
         </ul>
       </div>
-      <div>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+      <hr />
+      <div className="py-4">
+        <ul className="flex flex-col gap-4 mt-2">
+          <li className="flex gap-4">
+            <span>
+              <img src="./music.svg" alt="music" />
+            </span>
+            Music
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./sports.svg" alt="sports" />
+            </span>
+            Sports
+          </li>
+          <li className="flex gap-4">
+            <span>
+              <img src="./gaming.svg" alt="gaming" />
+            </span>
+            Gaming
+          </li>
+          
         </ul>
-      </div> */}
-    </div>
+      </div>
+    </div>):null
   );
 };
 
