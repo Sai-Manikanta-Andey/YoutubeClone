d293bb1c7cede7e6202281bb02fb369dc3d1f483
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-01-05 19:55:43 +0530

    https protocol add to yt search suggestions

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,10 +3,10 @@ export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +API_KEY;
 
 export const YOUTUBE_SUGGESTION_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" 
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" ; 
 
 export const YOUTUBE_LIVE_CHAT_API =
-  "https://youtube.googleapis.com/youtube/v3/liveChat/messages?key=" + API_KEY;
+  "https://youtube.googleapis.com/youtube/v3/liveChat/messages?key=AIzaSyDJV2XFQdFv0NwymBu5e7eNUUd3ewadG_c"
 
 //  https://youtube.googleapis.com/youtube/v3/search
 // ?part=snippet
@@ -16,6 +16,7 @@ export const YOUTUBE_LIVE_CHAT_API =
 // &regionCode=IN
 // &key=
 
+//https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=
 export const YOUTUBE_SEARCH_VIDEO_RESULTS_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=YOUR_SEARCH_TERM&type=video&maxResults=50&regionCode=IN&key=" +
   API_KEY;
