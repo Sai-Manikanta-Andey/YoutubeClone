6ed22beed6373ff9c4b463dfd4631dd17822922e
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-01-05 19:28:39 +0530

    unused imports remove

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./utils/store";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import VideoContainer from "./components/VideoContainer";
 import MainContainer from "./components/MainContainer";
 import WatchPage from "./components/WatchPage";
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,12 @@
 import React from "react";
 import SideBar from "./SideBar";
-import MainContainer from "./MainContainer";
 import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex gap-4 p-4 ">
       <SideBar />
-      <Outlet/>
+      <Outlet />
     </div>
   );
 };
