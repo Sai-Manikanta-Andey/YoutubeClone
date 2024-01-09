a650df427044161f892baa32377fff51cfe191c1
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-01-09 19:49:42 +0530

    unused url remove

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,18 +5,9 @@ export const YOUTUBE_API =
 export const YOUTUBE_SUGGESTION_API =
   "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&gl=IN&q="; 
 
-export const YOUTUBE_LIVE_CHAT_API =
-  "https://youtube.googleapis.com/youtube/v3/liveChat/messages?key=AIzaSyDJV2XFQdFv0NwymBu5e7eNUUd3ewadG_c"
 
-//  https://youtube.googleapis.com/youtube/v3/search
-// ?part=snippet
-// &q=YOUR_SEARCH_TERM  // Replace YOUR_SEARCH_TERM with the actual search term entered by the user
-// &type=video
-// &maxResults=50
-// &regionCode=IN
-// &key=
 
-//https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=
+
 export const YOUTUBE_SEARCH_VIDEO_RESULTS_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=YOUR_SEARCH_TERM&type=video&maxResults=50&regionCode=IN&key=" +
   API_KEY;
